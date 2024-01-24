f081f796d1f2a1770d26623ce56da1d568ad3cb6
kuusmaa <4j534j@gmail.com>
2024-01-24 19:36:04 +0200

    Update the Todo by id

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,9 +1,9 @@
-import express, {Request, Response, NextFunction} from 'express'
-import {Todo} from '../models/todo'
+import { Request, Response, NextFunction } from "express"
+import { Todo } from '../models/todo'
 
 const todos: Todo[] = []
 
-export const createTodo = (req: Request, res: Response, next: NextFunction) =>{
+export const createTodo = (req: Request, res:Response, next:NextFunction) => {
     try{
         const task = (req.body as {task: string}).task
         const newTodo = new Todo(Math.random().toString(), task)
@@ -11,18 +11,39 @@ export const createTodo = (req: Request, res: Response, next: NextFunction) =>{
         res.status(201).json({
             message: 'Created new todo',
             createdTask: newTodo
-        })
-    } catch(error){
+        }) 
+    } catch(error) {
         console.log(error)
     }
 }
 
-export const getTodos = (req: Request, res: Response, next: NextFunction) => {
+export const getTodos = (req: Request, res:Response, next: NextFunction) => {
     try{
         res.status(201).json({
             tasks: todos
         })
-    } catch(error){
+    } catch(error) {
         console.log(error)
     }
 }
+
+export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const todoId = req.params.id
+        const updatedTask = (req.body as {task:string}).task
+        const todoIndex = todos.findIndex(todo => todo.id === todoId)
+
+        if(todoIndex < 0){
+            throw new Error('Could not find todo with such id')
+        }
+        todos[todoIndex] = new Todo(todos[todoIndex].id, updatedTask)
+
+        res.status(201).json({
+            message: 'Todo is updated',
+            updatedTask: todos[todoIndex]
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+} 
